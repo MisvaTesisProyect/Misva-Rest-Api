38e263a7933621b5e67b5698a49dc5db3105d80e
Miguel Bonansea <47571076+MAbonansea@users.noreply.github.com>
2021-06-14 22:36:51 -0300

    Revert "JWT "

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,12 @@ import { AppService } from './app.service';
 import { MySqlDbModule } from './db/db.module';
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
-import { AuthModule } from './auth/auth.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     MySqlDbModule,
     UsersModule,
-    RolesModule,
-    AuthModule,
-    TypeOrmModule.forRoot(),
+    RolesModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { MySqlDbModule } from 'src/db/db.module';
-import { User } from './entities/user.entity';
-import { UsersService } from './service/users.service';
+import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
-  imports : [TypeOrmModule.forFeature([User]),MySqlDbModule],
   controllers: [UsersController],
   providers: [UsersService]
 })
