ba3dbbd70d0c2a6b06666b0f5b8e442c1f7985e8
Diaz Agustin <carlos.agustin.diaz1998@gmail.com>
2021-07-21 14:41:43 -0300

    create paginations in category

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, DefaultValuePipe, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { CategoriesService } from './services/categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { Category } from './entities/category.entity';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+
+/**
+ * El controlador de categorias tendra casi todo libre, salvo update o deletes.
+ */
 
 @Controller('categories')
 export class CategoriesController {
@@ -17,16 +24,29 @@ export class CategoriesController {
     return this.categoriesService.findAll();
   }
 
+  @Get('paginate')
+  async paginate(
+    @Body('page', new DefaultValuePipe(1), ParseIntPipe) page:number = 1,
+    @Body('limit', new DefaultValuePipe(20), ParseIntPipe) limit:number = 20
+  ):Promise<Pagination<Category>|any>{
+    /** 
+     * ? el limite maximo que va a tolerar va a ser 100, en caso que se necesite mas modificar.
+     */
+    limit =  limit > 100 ? 100 : limit
+    return this.categoriesService.findCategoryByPaginations({page,limit})
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.categoriesService.findOne(+id);
   }
-
+  
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
     return this.categoriesService.update(+id, updateCategoryDto);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.categoriesService.remove(+id);
--- src/categories/services/categories.service.ts
+++ src/categories/services/categories.service.ts
@@ -1,26 +1,80 @@
 import { Injectable } from '@nestjs/common';
+import { getRepository, Repository } from 'typeorm';
 import { CreateCategoryDto } from '../dto/create-category.dto';
 import { UpdateCategoryDto } from '../dto/update-category.dto';
-
+import { Category } from '../entities/category.entity';
+import { paginate,Pagination,IPaginationOptions,} from 'nestjs-typeorm-paginate'
+import { InjectRepository } from '@nestjs/typeorm';
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+
+  async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    try {
+      return await  getRepository(Category).save(createCategoryDto)
+    } catch (error) {
+      return error
+    }
+  }
+  async findCategoryByPaginations(options:IPaginationOptions): Promise<Pagination<Category>|any>{
+    return await paginate<Category>(getRepository(Category), options)
+  }
+  async findAll(): Promise<Category | any> {
+    try {
+      return await  getRepository(Category)
+                      .find({ relations: ['parent'] })
+                      .then(res =>{return res})
+    } catch (error) {
+      return error
+    }
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  /**
+   * 
+   * @param id int id categoria a buscar
+   * @returns 
+   */
+  async findOne(id: number): Promise<Category | any> {
+    try {
+      return await  getRepository(Category)
+                      .find({where:{id:id},
+                            relations:['parent']})
+                      .then(res => {return res})
+
+    } catch (error) {
+      
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  /**
+   * 
+   * @param id int id de la categoria
+   * @param updateCategoryDto Categoy datos a actualizar  
+   * @returns 
+   */
+  async update(id: number, updateCategoryDto: UpdateCategoryDto):Promise<Category|any> {
+       try {
+        return await getRepository(Category)
+          .createQueryBuilder("category")
+          .update(updateCategoryDto)
+          .where("id= :id",{id:id})
+          .execute()
+       } catch (error) {
+         return error
+       }
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  /**
+  * @param id int id de la categoria a eliminar
+   */
+  async remove(id: number):Promise<Category|any> {
+   try {
+    return await getRepository(Category)
+    .createQueryBuilder("category")
+    .delete()
+    .from(Category)
+    .where("id=:id",{id:id})
+   } catch (error) {
+     return error
+   }
   }
 }
